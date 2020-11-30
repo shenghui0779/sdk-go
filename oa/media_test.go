d8969fbcfda6f229bc8777cea7e2a533d305eb48
shenghui <shenghui@lanjingren.com>
2020-11-30 16:53:03 +0800

    add test

---
+++ oa/media_test.go
@@ -0,0 +1,39 @@
+package oa
+
+import (
+	"context"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/shenghui0779/gochat/wx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUploadMedia(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := wx.NewMockClient(ctrl)
+
+	client.EXPECT().Upload(gomock.AssignableToTypeOf(context.TODO()), "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=image", gomock.AssignableToTypeOf(postBody)).Return([]byte(`{
+		"errcode": 0,
+		"errmsg": "ok",
+		"type": "image",
+		"media_id": "MEDIA_ID",
+		"created_at": 1606717010
+	  }`), nil)
+
+	oa := New("APPID", "APPSECRET")
+	oa.client = client
+
+	dest := new(MediaUploadResult)
+
+	err := oa.Do(context.TODO(), "ACCESS_TOKEN", UploadMedia(MediaImage, "test.jpg", dest))
+
+	assert.Nil(t, err)
+	assert.Equal(t, &MediaUploadResult{
+		Type:      "image",
+		MediaID:   "MEDIA_ID",
+		CreatedAt: 1606717010,
+	}, dest)
+}
