9503bba4adcdd3de106517bf0d0de045f6b55318
shenghui <shenghui@lanjingren.com>
2020-12-01 11:10:01 +0800

    add new api

--- mp/other.go
+++ mp/other.go
@@ -1,27 +1,55 @@
 package mp
 
-import "github.com/shenghui0779/gochat/wx"
+import (
+	"encoding/json"
+	"net/url"
 
-// ServiceData 服务数据
-type ServiceData struct {
+	"github.com/shenghui0779/gochat/wx"
+	"github.com/tidwall/gjson"
+)
+
+// InvokeData 服务调用数据
+type InvokeData struct {
 	Service     string `json:"service"`       // 服务ID
 	API         string `json:"api"`           // 接口名
 	Data        wx.X   `json:"data"`          // 服务提供方接口定义的 JSON 格式的数据
 	ClientMsgID string `json:"client_msg_id"` // 随机字符串ID，调用方请求的唯一标识
 }
 
-// InvokeService 调用服务平台提供的服务
-func InvokeService() {
-
+type InvokeResult struct {
+	Data string `json:"data"`
 }
 
+// InvokeService 调用服务平台提供的服务
+func InvokeService(data *InvokeData, dest *InvokeResult) wx.Action {
+	return wx.NewOpenPostAPI(InvokeServiceURL, url.Values{}, wx.NewPostBody(func() ([]byte, error) {
+		return json.Marshal(data)
+	}), func(resp []byte) error {
+		dest.Data = gjson.GetBytes(resp, "data").String()
+
+		return nil
+	})
+}
+
+// SoterSignature 生物认证秘钥签名
 type SoterSignature struct {
 	OpenID        string `json:"open_id"`        // 用户 openid
 	JSONString    string `json:"json_string"`    // 通过 wx.startSoterAuthentication 成功回调获得的 resultJSON 字段
 	JSONSignature string `json:"json_signature"` // 通过 wx.startSoterAuthentication 成功回调获得的 resultJSONSignature 字段
 }
 
-// SoterVerify 生物认证秘钥签名验证
-func SoterVerify() {
+// SoterVerifyResult 生物认证秘钥签名验证结果
+type SoterVerifyResult struct {
+	OK bool
+}
 
+// SoterVerify 生物认证秘钥签名验证
+func SoterVerify(sign *SoterSignature, dest *SoterVerifyResult) wx.Action {
+	return wx.NewOpenPostAPI(SoterVerifyURL, url.Values{}, wx.NewPostBody(func() ([]byte, error) {
+		return json.Marshal(sign)
+	}), func(resp []byte) error {
+		dest.OK = gjson.GetBytes(resp, "is_ok").Bool()
+
+		return nil
+	})
 }
--- mp/other_test.go
+++ mp/other_test.go
@@ -1,1 +1,73 @@
 package mp
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/shenghui0779/gochat/wx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvokeService(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := wx.NewMockClient(ctrl)
+
+	client.EXPECT().Post(gomock.AssignableToTypeOf(context.TODO()), "https://api.weixin.qq.com/wxa/servicemarket?access_token=ACCESS_TOKEN", gomock.AssignableToTypeOf(postBody)).Return([]byte(`{
+		"errcode": 0,
+		"errmsg": "ok",
+		"data": "{\"idcard_res\":{\"type\":0,\"name\":{\"text\":\"abc\",\"pos\"…0312500}}},\"image_width\":480,\"image_height\":304}}",
+	}`), nil)
+
+	oa := New("APPID", "APPSECRET")
+	oa.client = client
+
+	data := &InvokeData{
+		Service: "wx79ac3de8be320b71",
+		API:     "OcrAllInOne",
+		Data: wx.X{
+			"img_url":   "http://mmbiz.qpic.cn/mmbiz_jpg/7UFjuNbYxibu66xSqsQqKcuoGBZM77HIyibdiczeWibdMeA2XMt5oibWVQMgDibriazJSOibLqZxcO6DVVcZMxDKgeAtbQ/0",
+			"data_type": 3,
+			"ocr_type":  1,
+		},
+		ClientMsgID: "id123",
+	}
+
+	dest := new(InvokeResult)
+
+	err := oa.Do(context.TODO(), "ACCESS_TOKEN", InvokeService(data, dest))
+
+	assert.Nil(t, err)
+	assert.Equal(t, `{"idcard_res":{"type":0,"name":{"text":"abc","pos"…0312500}}},"image_width":480,"image_height":304}}`, dest.Data)
+}
+
+func TestSoterVerify(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := wx.NewMockClient(ctrl)
+
+	client.EXPECT().Post(gomock.AssignableToTypeOf(context.TODO()), "https://api.weixin.qq.com/cgi-bin/soter/verify_signature?access_token=ACCESS_TOKEN", gomock.AssignableToTypeOf(postBody)).Return([]byte(`{
+		"errcode": 0,
+		"errmsg": "ok",
+		"is_ok": true
+	}`), nil)
+
+	oa := New("APPID", "APPSECRET")
+	oa.client = client
+
+	sign := &SoterSignature{
+		OpenID:        "$openid",
+		JSONString:    "$resultJSON",
+		JSONSignature: "$resultJSONSignature",
+	}
+
+	dest := new(SoterVerifyResult)
+
+	err := oa.Do(context.TODO(), "ACCESS_TOKEN", SoterVerify(sign, dest))
+
+	assert.Nil(t, err)
+	assert.True(t, dest.OK)
+}
