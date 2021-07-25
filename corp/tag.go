53d13fce9e689b93f33a9c42ebb3cf378f5fbda8
shenghui <shenghui0779@gmail.com>
2021-07-25 23:35:06 +0800

    标签管理

--- corp/tag.go
+++ corp/tag.go
@@ -2,6 +2,7 @@ package corp
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/tidwall/gjson"
 
@@ -25,4 +26,29 @@ func TagCreate(data *Tag) wx.Action {
 			return nil
 		}),
 	)
+}
+
+func TagUpdate(data *Tag) wx.Action {
+	return wx.NewPostAction(TagUpdateURL,
+		wx.WithBody(func() ([]byte, error) {
+			return json.Marshal(data)
+		}),
+	)
+}
+
+func TagDelete(tagID int) wx.Action {
+	return wx.NewGetAction(TagDeleteURL,
+		wx.WithQuery("tagid", strconv.Itoa(tagID)),
+	)
+}
+
+type TagUser struct {
+	UserID string `json:"userid"`
+	Name   string `json:"name"`
+}
+
+type TagSpec struct {
+	TagName   string     `json:"tagname"`
+	UserList  []*TagUser `json:"userlist"`
+	PartyList []int      `json:"partylist"`
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/mock v1.6.0
-	github.com/shenghui0779/yiigo v1.3.4-0.20210715151600-00dc6a67ab16
+	github.com/shenghui0779/yiigo v1.3.4-0.20210722155820-ec7108f9766b
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.8.1
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
