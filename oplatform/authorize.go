a47c20ed87115fd1c7b16ee1c6c82058de6e7400
a1192138141 <1192138141@qq.com>
2021-08-24 16:48:59 +0800

    代码格式娿

--- oplatform/authorize.go
+++ oplatform/authorize.go
@@ -54,30 +54,30 @@ type ComponentApiQueryAuth struct {
 type AuthorizationInfo struct {
 	AuthorizerAppid        string `json:"authorizer_appid"`
 	AuthorizerAccessToken  string `json:"authorizer_access_token"`
-	ExpiresIn              int64    `json:"expires_in"`
+	ExpiresIn              int64  `json:"expires_in"`
 	AuthorizerRefreshToken string `json:"authorizer_refresh_token"`
 }
 
 // 获取授权方的帐号基本信息
 // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/api_get_authorizer_info.html#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
 type ComponentApiGetAuthorizerInfo struct {
-	ComponentAccessToken string `json:"component_access_token"`
-	ComponentAppid string `json:"component_appid"`
-	AuthorizerAppid string `json:"authorizer_appid"`
-	AuthorizerInfo *AuthorizerInfo `json:"authorizer_info"`
-	AuthorizationInfo *AuthorizationInfo `json:"authorization_info"`
+	ComponentAccessToken string             `json:"component_access_token"`
+	ComponentAppid       string             `json:"component_appid"`
+	AuthorizerAppid      string             `json:"authorizer_appid"`
+	AuthorizerInfo       *AuthorizerInfo    `json:"authorizer_info"`
+	AuthorizationInfo    *AuthorizationInfo `json:"authorization_info"`
 }
 
 // TODO AuthorizerInfo 信息不完全
 type AuthorizerInfo struct {
-	NickName string `json:"nick_name"`
-	HeadImg string `json:"head_img"`
-	UserName string `json:"user_name"`
-	PrincipalName string `json:"principal_name"`
-	Alias string `json:"alias"`
-	QrcodeUrl string `json:"qrcode_url"`
+	NickName        string           `json:"nick_name"`
+	HeadImg         string           `json:"head_img"`
+	UserName        string           `json:"user_name"`
+	PrincipalName   string           `json:"principal_name"`
+	Alias           string           `json:"alias"`
+	QrcodeUrl       string           `json:"qrcode_url"`
 	ServiceTypeInfo *ServiceTypeInfo `json:"service_type_info"`
-	VerifyTypeInfo *VerifyTypeInfo `json:"verify_type_info"`
+	VerifyTypeInfo  *VerifyTypeInfo  `json:"verify_type_info"`
 }
 
 type ServiceTypeInfo struct {
@@ -91,15 +91,16 @@ type VerifyTypeInfo struct {
 // 获取刷新接口调用令牌
 // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/api_authorizer_token.html
 type ComponentApiAuthorizerToken struct {
-	ComponentAccessToken string `json:"component_access_token"`
-	AuthorizerAppid string `json:"authorizer_appid"`
+	ComponentAccessToken   string `json:"component_access_token"`
+	ComponentAppid         string `json:"component_appid"`
+	AuthorizerAppid        string `json:"authorizer_appid"`
 	AuthorizerRefreshToken string `json:"authorizer_refresh_token"`
-	ResponseInfo *ApiAuthorizerTokenResp
+	ResponseInfo           *ApiAuthorizerTokenResp
 }
 
 type ApiAuthorizerTokenResp struct {
-	AuthorizerAccessToken string `json:"authorizer_access_token"`
-	ExpiresIn int `json:"expires_in"`
+	AuthorizerAccessToken  string `json:"authorizer_access_token"`
+	ExpiresIn              int    `json:"expires_in"`
 	AuthorizerRefreshToken string `json:"authorizer_refresh_token"`
 }
 
@@ -136,24 +137,24 @@ func GetComponentApiQueryAuth(data *ComponentApiQueryAuth) wx.Action {
 			return json.Marshal(data)
 		}),
 		wx.WithDecode(func(resp []byte) error {
-			data.AuthorizationInfo.AuthorizerAppid =  gjson.GetBytes(resp, "authorization_info.authorizer_appid").String()
-			data.AuthorizationInfo.AuthorizerAccessToken =  gjson.GetBytes(resp, "authorization_info.authorizer_access_token").String()
-			data.AuthorizationInfo.ExpiresIn =  gjson.GetBytes(resp, "authorization_info.authorizer_access_token").Int()
-			data.AuthorizationInfo.AuthorizerRefreshToken =  gjson.GetBytes(resp, "authorization_info.authorizer_refresh_token").String()
+			data.AuthorizationInfo.AuthorizerAppid = gjson.GetBytes(resp, "authorization_info.authorizer_appid").String()
+			data.AuthorizationInfo.AuthorizerAccessToken = gjson.GetBytes(resp, "authorization_info.authorizer_access_token").String()
+			data.AuthorizationInfo.ExpiresIn = gjson.GetBytes(resp, "authorization_info.authorizer_access_token").Int()
+			data.AuthorizationInfo.AuthorizerRefreshToken = gjson.GetBytes(resp, "authorization_info.authorizer_refresh_token").String()
 			return nil
 		}),
 	)
 }
 
 // 获取授权方的帐号基本信息
-func GetComponentApiGetAuthorizerInfo(data *ComponentApiGetAuthorizerInfo) wx.Action  {
+func GetComponentApiGetAuthorizerInfo(data *ComponentApiGetAuthorizerInfo) wx.Action {
 	return wx.NewPostAction(fmt.Sprintf(urls.ComponentApiGetAuthorizerInfoUrl, data.ComponentAccessToken),
 		wx.WithBody(func() ([]byte, error) {
 			return json.Marshal(data)
 		}),
 		wx.WithDecode(func(resp []byte) error {
-			data.AuthorizerInfo.ServiceTypeInfo = &ServiceTypeInfo{Id:gjson.GetBytes(resp, "authorizer_info.service_type_info.id").Int()}
-			data.AuthorizerInfo.VerifyTypeInfo = &VerifyTypeInfo{Id:gjson.GetBytes(resp, "authorizer_info.verify_type_info.id").Int()}
+			data.AuthorizerInfo.ServiceTypeInfo = &ServiceTypeInfo{Id: gjson.GetBytes(resp, "authorizer_info.service_type_info.id").Int()}
+			data.AuthorizerInfo.VerifyTypeInfo = &VerifyTypeInfo{Id: gjson.GetBytes(resp, "authorizer_info.verify_type_info.id").Int()}
 			data.AuthorizerInfo.NickName = gjson.GetBytes(resp, "authorizer_info.nick_name").String()
 			data.AuthorizerInfo.HeadImg = gjson.GetBytes(resp, "authorizer_info.head_img").String()
 			data.AuthorizerInfo.UserName = gjson.GetBytes(resp, "authorizer_info.user_name").String()
@@ -161,19 +162,19 @@ func GetComponentApiGetAuthorizerInfo(data *ComponentApiGetAuthorizerInfo) wx.Ac
 			data.AuthorizerInfo.Alias = gjson.GetBytes(resp, "authorizer_info.alias").String()
 			data.AuthorizerInfo.QrcodeUrl = gjson.GetBytes(resp, "authorizer_info.qrcode_url").String()
 
-			data.AuthorizationInfo.AuthorizerAppid =  gjson.GetBytes(resp, "authorization_info.authorizer_appid").String()
-			data.AuthorizationInfo.AuthorizerAccessToken =  gjson.GetBytes(resp, "authorization_info.authorizer_access_token").String()
-			data.AuthorizationInfo.ExpiresIn =  gjson.GetBytes(resp, "authorization_info.authorizer_access_token").Int()
-			data.AuthorizationInfo.AuthorizerRefreshToken =  gjson.GetBytes(resp, "authorization_info.authorizer_refresh_token").String()
+			data.AuthorizationInfo.AuthorizerAppid = gjson.GetBytes(resp, "authorization_info.authorizer_appid").String()
+			data.AuthorizationInfo.AuthorizerAccessToken = gjson.GetBytes(resp, "authorization_info.authorizer_access_token").String()
+			data.AuthorizationInfo.ExpiresIn = gjson.GetBytes(resp, "authorization_info.authorizer_access_token").Int()
+			data.AuthorizationInfo.AuthorizerRefreshToken = gjson.GetBytes(resp, "authorization_info.authorizer_refresh_token").String()
 			return nil
 		}),
-		)
+	)
 }
 
 // 获取/刷新接口调用令牌
 func GetComponentApiAuthorizertoken(data *ComponentApiAuthorizerToken) wx.Action {
 	return wx.NewPostAction(urls.ComponentApiGetAuthorizerTokenUrl,
-		wx.WithQuery("component_access_token",data.ComponentAccessToken),
+		wx.WithQuery("component_access_token", data.ComponentAccessToken),
 		wx.WithBody(func() ([]byte, error) {
 			return json.Marshal(data)
 		}),
