cabfe82fedc7515925754c594141b0492cbe21de
shenghui <shenghui0779@gmail.com>
2021-11-13 22:20:56 +0800

    统一小程序Action参数

--- minip/qrcode.go
+++ minip/qrcode.go
@@ -69,14 +69,14 @@ func GetQRCode(params *ParamsQRCodeGet, qrcode *QRCode) wx.Action {
 }
 
 type ParamsQRCodeUnlimit struct {
-	Scene      string     `json:"scene"`                // 最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
-	Page       string     `json:"page"`                 // 页面 page，例如 pages/index/index，根路径前不要填加 /，不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面
-	CheckPath  *bool      `json:"check_path"`           // 检查page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但page 有数量上限（60000个）请勿滥用
-	EnvVersion EnvVersion `json:"env_version"`          // 要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"
-	Width      int        `json:"width,omitempty"`      // 二维码的宽度，单位 px。最小 280px，最大 1280px
-	AutoColor  bool       `json:"auto_color,omitempty"` // 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
-	LineColor  *RGB       `json:"line_color,omitempty"` // auto_color 为 false 时生效，使用 rgb 设置颜色，十进制表示，默认：{"r":0,"g":0,"b":0}
-	IsHyaline  bool       `json:"is_hyaline,omitempty"` // 是否需要透明底色，为 true 时，生成透明底色的小程序码
+	Scene      string     `json:"scene"`                 // 最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
+	Page       string     `json:"page,omitempty"`        // 页面 page，例如 pages/index/index，根路径前不要填加 /，不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面
+	CheckPath  *bool      `json:"check_path,omitempty"`  // 检查page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但page 有数量上限（60000个）请勿滥用
+	EnvVersion EnvVersion `json:"env_version,omitempty"` // 要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"
+	Width      int        `json:"width,omitempty"`       // 二维码的宽度，单位 px。最小 280px，最大 1280px
+	AutoColor  bool       `json:"auto_color,omitempty"`  // 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+	LineColor  *RGB       `json:"line_color,omitempty"`  // auto_color 为 false 时生效，使用 rgb 设置颜色，十进制表示，默认：{"r":0,"g":0,"b":0}
+	IsHyaline  bool       `json:"is_hyaline,omitempty"`  // 是否需要透明底色，为 true 时，生成透明底色的小程序码
 }
 
 // GetUnlimitQRCode 获取小程序二维码（数量不限）
