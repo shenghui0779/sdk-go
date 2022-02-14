07301f01e4d90566032258bfaec1021b7e04686a
shenghui <shenghui0779@gmail.com>
2022-02-14 16:56:59 +0800

    更新小程序消息

--- minip/message.go
+++ minip/message.go
@@ -69,8 +69,8 @@ func SendUniformMsg(touser string, msg *TemplateMsg) wx.Action {
 	)
 }
 
-// ParamsSubscribeMsg 订阅消息参数
-type ParamsSubscribeMsg struct {
+// SubscribeMsg 订阅消息参数
+type SubscribeMsg struct {
 	ToUser     string       `json:"touser"`                      // 接收者（用户）的 openid
 	TemplateID string       `json:"template_id"`                 // 所需下发的订阅模板ID
 	Page       string       `json:"page,omitempty"`              // 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转
@@ -80,10 +80,10 @@ type ParamsSubscribeMsg struct {
 }
 
 // SendSubscribeMsg 订阅消息 - 发送订阅消息
-func SendSubscribeMsg(params *ParamsSubscribeMsg) wx.Action {
+func SendSubscribeMsg(msg *SubscribeMsg) wx.Action {
 	return wx.NewPostAction(urls.MinipSubscribeMsgSend,
 		wx.WithBody(func() ([]byte, error) {
-			return json.Marshal(params)
+			return json.Marshal(msg)
 		}),
 	)
 }
