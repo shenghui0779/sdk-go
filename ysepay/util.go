886176a9e101c7480a96dd017399d556b9c0b803
shenghui <shenghui0779@gmail.com>
2024-01-18 15:52:20 +0800

    Update util.go

--- ysepay/util.go
+++ ysepay/util.go
@@ -1,14 +1,6 @@
 package ysepay
 
-import (
-	"crypto/tls"
-	"encoding/pem"
-	"errors"
-	"os"
-	"path/filepath"
-
-	"golang.org/x/crypto/pkcs12"
-)
+import "errors"
 
 // ErrSysAccepting 网关受理中
 var ErrSysAccepting = errors.New("SYS001 | 网关受理中")
@@ -20,31 +12,3 @@ const (
 	ComOK         = "COM000" // 业务受理成功
 	ComProcessing = "COM004" // 业务处理中
 )
-
-// LoadCertFromPfxFile 通过pfx(p12)证书文件生成TLS证书
-// 注意：证书需采用「TripleDES-SHA1」加密方式
-func LoadCertFromPfxFile(filename, password string) (tls.Certificate, error) {
-	fail := func(err error) (tls.Certificate, error) { return tls.Certificate{}, err }
-
-	certPath, err := filepath.Abs(filepath.Clean(filename))
-	if err != nil {
-		return fail(err)
-	}
-
-	pfxdata, err := os.ReadFile(certPath)
-	if err != nil {
-		return fail(err)
-	}
-
-	blocks, err := pkcs12.ToPEM(pfxdata, password)
-	if err != nil {
-		return fail(err)
-	}
-
-	pemData := make([]byte, 0)
-	for _, b := range blocks {
-		pemData = append(pemData, pem.EncodeToMemory(b)...)
-	}
-
-	return tls.X509KeyPair(pemData, pemData)
-}
